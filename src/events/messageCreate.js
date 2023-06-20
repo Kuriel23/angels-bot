bf412f2b5a0a38eda9884a6e3e2ffe68a5a91149
Kuriel <37979213+Kuriel23@users.noreply.github.com>
2023-06-20 19:50:47 +0100

    feat: message of new partner

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,7 +1,79 @@
+const discord = require("discord.js");
+
 module.exports = async (client, message) => {
 	if (message.guild === null) return;
 
 	if (message.author.bot) return 0;
+	if (message.channel.id === "") {
+		const doc = await client.db.PartnersStaff.findOne({
+			_id: message.author.id,
+		});
+		if (doc) {
+			doc.partners += 1;
+			doc.save();
+		} else {
+			new client.db.PartnersStaff({
+				_id: message.author.id,
+				partners: 1,
+			}).save();
+		}
+		function extractInfo(message) {
+			const inviteLinkRegex =
+				/https?:\/\/discord(?:app)?\.com\/invite\/([a-zA-Z0-9]+)/;
+			const representativeRegex = /<@!\d+>/g;
+			const inviteLinkMatch = message.match(inviteLinkRegex);
+			const representativeMatch = message.match(representativeRegex);
+			const inviteLink = inviteLinkMatch ? inviteLinkMatch[0] : null;
+			const inviteCode = inviteLinkMatch ? inviteLinkMatch[1] : null;
+			const representative = representativeMatch
+				? representativeMatch[0]
+				: null;
+			return { inviteLink, inviteCode, representative };
+		}
+		const info = extractInfo(message);
+		let name = "Desconhecido";
+		await client.fetchInvite(info.inviteCode).then((invite) => {
+			name = invite.name;
+		});
+		message.reply({
+			components: [
+				new discord.ActionRowBuilder().setComponents(
+					new discord.ButtonBuilder()
+						.setStyle(discord.ButtonStyle.Link)
+						.setLabel("Entrar")
+						.setURL(info.inviteLink)
+				),
+			],
+			content: "<@&1114235343093375067>",
+			embeds: [
+				{
+					title: `Obrigado(a) pela parceria!`,
+					description: `${message.author.toString()} fez uma parceria com ${
+						info.representative
+					} (\`${info.representative
+						.replace("<@", "")
+						.replace(
+							">",
+							""
+						)}\`) do servidor ${name} e agora possui ${
+						doc.partners
+					} parceria feitas.`,
+					footer: {
+						text: message.guild.name,
+						iconURL: message.guild.iconURL({ format: "png" }),
+					},
+					author: {
+						name: message.author.username,
+						iconURL: message.author.iconURL({ format: "png" }),
+					},
+					color: "5814783",
+					image: {
+						url: "https://cdn.discordapp.com/attachments/1114043735970418748/1116456127308255242/188_Sem_Titulo_20230608165727.png",
+					},
+				},
+			],
+		});
+	}
 	if (
 		(message.content.startsWith("ag?") &&
 			message.author.id === "354233941550694400") ||
